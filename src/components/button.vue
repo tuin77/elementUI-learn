97c07a69cd34640ec3d09ac3b4ae5cea344b5325
yanqi <yanqi2020@qq.com>
2022-03-14 23:58:38 +0800

    feat:add component button.

--- src/components/button.vue
+++ src/components/button.vue
@@ -1,27 +1,81 @@
 <template>
-  <button class="my-button"><slot></slot></button>
+  <button
+    class="el-button"
+    @click="handleClick"
+    :disabled="buttonDisabled || loading"
+    :autofocus="autofocus"
+    :type="nativeType"
+    :class="[
+      type ? 'el-button--' + type : '',
+      buttonSize ? 'el-button--' + buttonSize : '',
+      {
+        'is-disabled': buttonDisabled,
+        'is-loading': loading,
+        'is-plain': plain,
+        'is-round': round,
+        'is-circle': circle
+      }
+    ]"
+  >
+    <i class="el-icon-loading" v-if="loading"></i>
+    <i :class="icon" v-if="icon && !loading"></i>
+    <span v-if="$slots.default"><slot></slot></span>
+  </button>
 </template>
-
 <script>
 export default {
-  props:{
-    type:{
-      type:String
-    },
-    size:{
-      type:String
-    },
-    round:{
-      type:Boolean
-    },
-    circle:{
-      type:Boolean
-    }
-  }
+  name: 'ElButton',
 
-}
+  inject: {
+    elForm: {
+      default: '',
+    },
+    elFormItem: {
+      default: '',
+    },
+  },
+
+  props: {
+    type: {
+      type: String,
+      default: 'default',
+    },
+    size: String,
+    icon: {
+      type: String,
+      default: '',
+    },
+    nativeType: {
+      type: String,
+      default: 'button',
+    },
+    loading: Boolean,
+    disabled: Boolean,
+    plain: Boolean,
+    autofocus: Boolean,
+    round: Boolean,
+    circle: Boolean,
+  },
+
+  computed: {
+    elFormItemSize() {
+      return (this.elFormItem || {}).elFormItemSize;
+    },
+    buttonSize() {
+      return this.size || this.elFormItemSize || (this.$ELEMENT || {}).size;
+    },
+    buttonDisabled() {
+      return this.disabled || (this.elForm || {}).disabled;
+    },
+  },
+
+  methods: {
+    handleClick(evt) {
+      this.$emit('click', evt);
+    },
+  },
+};
 </script>
-
-<style>
-
+<style lang="scss">
+@import "./button.scss"
 </style>
