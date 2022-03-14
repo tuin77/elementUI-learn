ef9e7d9c3e624109f1b6a0a15f89de5f39e1e441
yanqi <yanqi2020@qq.com>
2022-03-14 20:01:48 +0800

    fix:init project.

---
+++ src/components/button.vue
@@ -0,0 +1,27 @@
+<template>
+  <button class="my-button"><slot></slot></button>
+</template>
+
+<script>
+export default {
+  props:{
+    type:{
+      type:String
+    },
+    size:{
+      type:String
+    },
+    round:{
+      type:Boolean
+    },
+    circle:{
+      type:Boolean
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
